25fa49df17ce30184af5c4e39d1748d911ce32b0
Michael Harwood <harwood@quarkphysics.ca>
2022-03-29 20:27:52 -0400

    fixed <Title>

--- adminOneUser.php
+++ adminOneUser.php
@@ -40,7 +40,7 @@ if ($stmt = $db->prepare($sql)) {
 
 <head>
     <meta charset="utf-8">
-    <title>File Uploader: Admin (one user)</title>
+    <title>File Uploader: Admin (<?=$stFullname ?>)</title>
     <link rel="stylesheet" href="./resources/bootstrap.min.css">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<script src="./resources/jquery.3.4.1.min.js"></script>
